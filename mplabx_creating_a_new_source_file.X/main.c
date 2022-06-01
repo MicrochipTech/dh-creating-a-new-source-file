2bcaced83f4e4c90bdbf44d8194a4680da8d91f7
mchpyyz <91680378+mchpyyz@users.noreply.github.com>
2022-06-01 15:11:59 -0400

    Added project

---
+++ mplabx_creating_a_new_source_file.X/main.c
@@ -0,0 +1,14 @@
+/*
+ * File:   main.c
+ * Author: root
+ *
+ * Created on April 21, 2022, 1:59 PM
+ */
+
+
+#include <xc.h>
+#include "configBits.h"
+
+void main(void) {
+    return;
+}
---
+++ mplabx_creating_a_new_source_file.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=mplabx_creating_a_new_source_file.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/mplabx_creating_a_new_source_file.X.production.hex
